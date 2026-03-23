9a221bba30b1a3946492e629efb2d8c8ac8469b7
TankistPro <nakol.nayk@mail.ru>
2026-03-23 22:13:45 +0300

    fix: add passwordHash field to User model

--- gen/user.ts
+++ gen/user.ts
@@ -42,6 +42,7 @@ export interface User {
   username: string;
   email: string;
   avatarUrl: string;
+  passwordHash: string;
   createdAt: Timestamp | undefined;
   updatedAt: Timestamp | undefined;
 }
